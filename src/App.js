70cafc7ada92bdd0eef19eadbcf6db75458c0a12
Hasan Tokatlı Edu <hasantktledu@gmail.com>
2024-08-11 18:31:05 +0300

    Çoklu form örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [degerler, degerleriGuncelle] = useState({})
+
+  function inputDegistir(olay) {
+    const name = olay.target.name;
+    const value = olay.target.value;
+    degerleriGuncelle(degerler => ( {...degerler, [name]: value}) )
+  }
+
   return (
-    <div>
-      App 
-    </div>
-  );
+    <>
+      <div>Veri input değeri: {degerler.ad} {degerler.soyad} {degerler.telefon}</div>
+
+      <input name="ad" value={degerler.ad || ""} onChange={inputDegistir} placeholder="Ad" />
+      <input name="soyad" value={degerler.soyad || ""}  onChange={inputDegistir} placeholder="Soyad" />
+      <input name="telefon" value={degerler.telefon || ""} onChange={inputDegistir} placeholder="Telefon" />
+      <input name="eposta" value={degerler.eposta || ""} onChange={inputDegistir} placeholder="Eposta" />
+    </>
+  )
 }
 
 export default App;
